e9f03d6f08be6a9c4176792378968dc861f3c886
Brian Anderson <46289694+Actane727@users.noreply.github.com>
2019-08-26 15:35:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,22 @@ fi
 
 #Step 2) Update repository----------------------------------
 sudo apt-get update -y
+sudo apt-get upgrade -y
 
-#Step 3) Install modules and install/enable VNC
-sudo apt-get install python-pip -y
-pip install openpyxl
+#Step 3) Install software 
+sudo apt-get install libreoffice
+sudo apt-get install python-pip
 sudo apt-get install xterm
 sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer
+
+#Step 4) Install openpyxl
+pip install openpyxl
+
+#Step 5) Enable VNC Server
 sudo ln -s /usr/lib/systemd/system/vncserver-x11-serviced.service /etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service
 sudo systemctl start vncserver-x11-serviced
 
-#Step 4) Download Python script-----------------------------
+#Step 6) Download Python script-----------------------------
 cd /opt/
 sudo mkdir -p TimeFiles
 cd /opt/TimeFiles
@@ -37,7 +43,7 @@ wget -N -q --show-progress "https://raw.githubusercontent.com/Actane727/TimeTrac
 
 #-----------------------------------------------------------
 
-#Step 4) Run script in terminal at startup------------
+#Step 7) Run script in terminal at startup------------
 cd /home/pi/.config
 mkdir autostart
 cd /autostart
@@ -50,7 +56,7 @@ else
   echo "[Desktop Entry]\nType=Application\nName=Time\nExec=xterm -hold -maximized -e \"/usr/bin/python3 /opt/TimeFiles/PunchTime.py" >> $TD
 fi
 
-#Step 5) Reboot to apply changes----------------------------
+#Step 8) Reboot to apply changes----------------------------
 echo "Punch Time Program installation done. Will now reboot after 3 seconds."
 sleep 3
 sudo reboot
