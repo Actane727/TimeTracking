3ad73a3c3afdf64c4500563803e35e58a5e62e60
Brian Anderson <46289694+Actane727@users.noreply.github.com>
2019-08-27 12:59:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,9 @@ sudo apt-get install python3 -y
 sudo apt-get install xterm -y
 sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer -y
 
-#Step 4) Install openpyxl
+#Step 4) Install openpyxl and make file
 sudo apt install python3-openpyxl -y
+touch /home/pi/Desktop/TrainingTime.xlsx
 
 #Step 5) Enable VNC Server
 sudo ln -s /usr/lib/systemd/system/vncserver-x11-serviced.service /etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service
