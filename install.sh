58696f1c7a6900bd43bf4e119bc1dd0702b563c8
Brian Anderson <46289694+Actane727@users.noreply.github.com>
2019-09-09 13:36:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ sudo apt install realvnc-vnc-server realvnc-vnc-viewer -y
 sudo apt install python3-openpyxl -y
 
 #Step 5) Enable VNC Server
-sudo ln -s /usr/lib/systemd/system/vncserver-x11-serviced.service /etc/systemd/system/multi-user.target.wants/vncserver-x11-serviced.service
 sudo systemctl start vncserver-x11-serviced
 
 #Step 6) Download Python scripts-----------------------------
@@ -47,6 +46,9 @@ mkdir /home/pi/.config/lxsession
 mkdir /home/pi/.config/lxsession/LXDE-pi
 sudo mv autostart /home/pi/.config/lxsession/LXDE-pi
 
+#Step 8) Clean-up changes
+cd
+sudo apt-get autoremove -y
 
 #Step 8) Reboot to apply changes----------------------------
 echo "Punch Time Program installation done. Will now reboot after 3 seconds."
