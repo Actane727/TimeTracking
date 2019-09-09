7a0e32842e60d299ead088d7e3823511ed2dfdae
Brian Anderson <46289694+Actane727@users.noreply.github.com>
2019-09-09 10:55:36 -0500

    Update punchtime.py

--- punchtime.py
+++ punchtime.py
@@ -7,9 +7,10 @@ import os
 import os.path
 from time import sleep
 
-filepath = "/home/pi/Desktop/TrainingTime.xlsx" 	# -Filepath to the running document
-# condition = 1	# This helps to keep from saving a blank document to the Desktop
-logs = 0	# This is to only save the data sheet after a certain number of punch-in times.
+filepath = "/home/pi/Desktop/TrainingTime.xlsx"  # -Filepath to the running document
+logs = 0  # This is to only save the data sheet after a certain number of punch-in times.
+oldtime = 0
+persold = 0
 
 if os.path.isfile(filepath):
     print("")
@@ -17,14 +18,13 @@ else:
     wb = Workbook()
     wb.save(filepath)
 
-warnings.simplefilter("ignore") 	# -----------------Ignoring errors that come up on initial boot
-wb = load_workbook(filepath)	# ---------------------Running workbook
-warnings.simplefilter("default")	# -----------------Returning the warning back on to normal after boot
-ws = wb.active	# -------------------------------------Making Sheet1 active sheet
+warnings.simplefilter("ignore")  # ------------------Ignoring errors that come up on initial boot
+wb = load_workbook(filepath)  # ---------------------Running workbook
+warnings.simplefilter("default")  # -----------------Returning the warning back on to normal after boot
+ws = wb.active  # -----------------------------------Making Sheet1 active sheet
 
 
-
-# /////////////////////////////////////////////Functions and Definitions//////////////////////////////////////////////////
+# /////////////////////////////////////////////Functions and Definitions////////////////////////////////////////////////
 def save():     # This is the standard save function
     print("-" * 79 + "\nThe file has been saved at " + dt.datetime.now().strftime('%m/%d/%y %H:%M') + ".\n")
     wb.save(filepath)
@@ -50,10 +50,11 @@ def clearsave():    # Save the document to Desktop with date, then create a clea
         condition = 1
     else:
         print("/" * 79)
-        print("**The saved document is already at the most current document and saving now will create a blank document.")
-        print ("/" * 79 + "\n")
+        print("**The saved document is already at the most current document. Saving now will create a blank document.")
+        print("/" * 79 + "\n")
 
-def restart():	# Restarting for changes to take effect for ease of use
+
+def restart():  # Restarting for changes to take effect for ease of use
     wb.save(filepath)
     answer = input("Are you sure that you want to reboot now? y/n ")
     if answer == "n":
@@ -73,41 +74,55 @@ def restart():	# Restarting for changes to take effect for ease of use
 def main():
     global condition
     global logs
+    global oldtime
+    global persold
     while True:
         print("Please swipe your card to sign in to the Maintenance Department Training Room.")
         print("-" * 14 + "Type \"save\" to quick save or \"reboot\" to reboot Pi" + "-" * 14 + "\n")
-        persNum = input()
-        if persNum == "reboot":
+        persnum = input()
+        if persnum == "reboot":
             restart()
             continue
-        elif persNum == "save":
+        elif persnum == "save":
             justsave()
             continue
-        elif persNum == "0900383671":
+        elif persnum == "0900383671":
             clearsave()
             continue
-        elif persNum == "0900754894":
+        elif persnum == "0900754894":
             clearsave()
             continue
         else:
             try:
-                int(persNum)
+                int(persnum)
                 pass
             except ValueError:
-                print(persNum + " is not a valid personnel number!\n")
+                print(persnum + " is not a valid personnel number!\n")
                 continue
-            input_row = wb.active.max_row + 1
-            ws['B{}'.format(input_row)] = int(persNum)
-            ws['C{}'.format(input_row)] = dt.datetime.now().strftime('%-m/%-d/%y %l:%M:00 %p')
-            print("Hello, your personnel number is " + str(persNum))
-            if logs >= 5:
-                print("Your data is saved on row " + str(input_row), "at " +
-                                        str(dt.datetime.now().strftime('%m/%d/%y %l:%M %p')) + ".")
+        if persnum == persold:
+            diff = dt.datetime.now() - oldtime
+            if diff.total_seconds() <= 120:
+                print(diff)
+                continue
             else:
-                print("Your data is saved on row " + str(input_row), "at " +
-                                        str(dt.datetime.now().strftime('%m/%d/%y %l:%M %p')) + ".\n")
-            logs += 1
-            condition = 0
+                print(diff)
+                pass
+        else:
+            pass
+        input_row = wb.active.max_row + 1
+        ws['B{}'.format(input_row)] = int(persnum)
+        ws['C{}'.format(input_row)] = dt.datetime.now().strftime('%-m/%-d/%y %l:%M:00 %p')
+        print("Hello, your personnel number is " + str(persnum))
+        if logs >= 5:
+            print("Your data is saved on row " + str(input_row), "at " +
+                  str(dt.datetime.now().strftime('%m/%d/%y %l:%M %p')) + ".")
+        else:
+            print("Your data is saved on row " + str(input_row), "at " +
+                  str(dt.datetime.now().strftime('%m/%d/%y %l:%M %p')) + ".\n")
+        logs += 1
+        condition = 0
+        oldtime = dt.datetime.now()
+        persold = persnum
         if logs >= 5:
             save()
             logs = 0
